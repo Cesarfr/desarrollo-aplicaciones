07593bcce8d57d957c9054905907b1af07d92232
Cesar Hernandez Antonio <hernandez.antonio.cesar19@gmail.com>
2016-06-15 13:35:04 -0500

    07-MensajesEnPull: Proyecto creado

---
+++ 07-MensajesEnPull/src/vista/VAgregarMensaje.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class VAgregarMensaje extends JFrame {
+	
+	private JLabel lbNombre = new JLabel("Nombre: GG");
+	private JTextField txtMensaje = new JTextField();
+	private JTextArea taMensajes = new JTextArea();
+	
+}
---
+++ 07-MensajesEnPull/src/vista/VInicio.java
@@ -0,0 +1,20 @@
+package vista;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class VInicio extends JFrame {
+	
+	private JLabel lbMensaje = new JLabel("Bienvenido al \"Mensajito\"");
+	private JLabel lbInicio = new JLabel("INICIAR SESIÓN");
+	private JLabel lbUser = new JLabel("Usuario:");
+	private JTextField txtUser = new JTextField();
+	private JLabel lbPasswd = new JLabel("Contraseña:");
+	private JPasswordField txtPasswd = new JPasswordField();
+	private JButton btnAcceder = new JButton("Acceder");
+//	private JButton 
+	
+}
